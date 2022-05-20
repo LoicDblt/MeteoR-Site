324f8fa066cce24e2f33b9b7b52e2b5b5ac704ec
Loïc DIEBOLT <loic@dblt.fr>
2022-05-20 14:42:42 +0200

    Mise à jour page d'erreur

--- erreur_404.php
+++ erreur_404.php
@@ -11,16 +11,54 @@
 	<meta name="description" content="Page non trouvée"/>
 	<link rel="icon" type="image/webp" href="img/meteor_favicon.webp"/>
 	<link rel="apple-touch-icon" href="img/meteor_apple_touch.webp">
-	<style>
-body{
-	background: url("img/erreur_404.jpg");
-	background-size: cover;
-	background-attachment: fixed;
-	background-repeat: no-repeat;
-	background-position: center center;
-}
-	</style>
+	<link rel="stylesheet" type="text/css" href="style/style.css"/>
 </head>
 <body>
+	<p>
+		Erreur 404
+		<br>
+		Page introuvable
+	</p>
 </body>
+<style>
+@font-face {
+	font-family: "Open Sans";
+	font-display: swap;
+	src: url("style/polices/open-sans-600.woff2") format("woff2"),
+		url("style/polices/open-sans-600.woff") format("woff");
+}
+body{
+	width: 100vw;
+	height: 100vh;
+	margin: 0;
+	font-family: "Open Sans", Arial, sans-serif;
+	background: url("img/erreur_404.jpg");
+	background-size: cover;
+	background-position: center center;
+}
+p{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	color: #ffffff;
+	font-size: 34px;
+	margin: 0;
+	height: 100%;
+}
+
+/* Version tablette */
+@media only screen and (min-width: 800px) and (max-width: 1080px){
+	p{
+		font-size: 28px;
+	}
+}
+
+/* Version mobile */
+@media screen and (max-width: 799px){
+	p{
+		font-size: 24px;
+	}
+}
+</style>
 </html>
